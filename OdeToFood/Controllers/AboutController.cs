fc4dd1f6c0147b190c85d0dff9fe1ecdc7675855
Rex Zhiheng He <rex@rex-he.net>
2018-12-24 20:24:31 +1100

    Attribute Routes

--- OdeToFood/Controllers/AboutController.cs
+++ OdeToFood/Controllers/AboutController.cs
@@ -1,17 +1,21 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace OdeToFood.Controllers
 {
+    [Route("company/[controller]")] // Use token here.
     public class AboutController
-    { 
+    {
+        [Route("")]
         public string Phone()
         {
             return "8888 1111";
         }
 
+        [Route("[action]")]
         public string Address()
         {
             return "AU";
