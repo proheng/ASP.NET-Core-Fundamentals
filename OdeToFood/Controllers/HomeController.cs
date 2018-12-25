b8170f5191a5ba65e31d6fa9d0e74a7575a26823
Rex Zhiheng He <rex@rex-he.net>
2018-12-25 19:03:44 +1100

    Models and View Models

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -1,21 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using OdeToFood.Models;
 using OdeToFood.Services;
+using OdeToFood.ViewModels;
 
 namespace OdeToFood.Controllers
 {
     public class HomeController : Controller
     {
         private IRestaurantData _restaurantData;
+        private IGreeter _greet;
 
-        public HomeController(IRestaurantData restaurantData)
+        public HomeController(
+            IRestaurantData restaurantData,
+            IGreeter greeter)
         {
             _restaurantData = restaurantData;
+            _greet = greeter;
         }
         
         public IActionResult Index()
         {
-            var model = _restaurantData.GetAll();
+            var model = new HomeIndexViewModel
+            {
+                Restaurants = _restaurantData.GetAll(),
+                CurrentMessage = _greet.GetMessageOfTheDay()
+            };
             return View(model);
         }
     }
