d0d608bcca502d5f95f226323cbc3ac1135ebcc1
Rex Zhiheng He <rex@rex-he.net>
2018-12-25 14:32:22 +1100

    Action Result

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -1,10 +1,14 @@
-﻿namespace OdeToFood.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using OdeToFood.Models;
+
+namespace OdeToFood.Controllers
 {
-    public class HomeController
+    public class HomeController : Controller
     {
-        public string Index()
+        public IActionResult Index()
         {
-            return "Hello from the HomeController";
+            var model = new Restaurant() { Id = 1, Name = "Rex's Pizza Place" };
+            return new ObjectResult(model);
         }
     }
 }
